8204f276303ec785278f3b21905679a9f4083d7b
junhwi <wjdrhdsbrlfl@gmail.com>
2025-07-31 10:04:21 +0900

    [feature] check devide by zero error

--- calc.py
+++ calc.py
@@ -1,4 +1,6 @@
 class Calc:
 
-    def getDevide(self, numerator, denominator):
+    def getDivide(self, numerator, denominator):
+        if denominator == 0:
+            raise ValueError("Can't divide by zero")
         return numerator/denominator
